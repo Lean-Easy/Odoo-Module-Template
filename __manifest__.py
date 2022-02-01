e784276eb2d9831757568c64a2c689edb85ef40c
ConorEganLE <conor@leaneasy.co.uk>
2022-02-01 14:42:25 +0000

    init

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'MODULE_NAME',
+    'version': '1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'SHORT_DESCRIPTION',
+    'author': "AUTHOR",
+    'website': "WEBSITE",
+    'depends': ['base'],
+    'data': [
+        # Uncomment below if needed
+        # 'security/ir.model.access.csv',
+        # 'views/module_name_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'OPL-1',
+}
---
+++ models/module_name.py
@@ -0,0 +1,6 @@
+from odoo import api, fields, models, _
+
+
+class ModelName(models.Model):
+    # _inherit = ''
+    _name = "model.name" 
